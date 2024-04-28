4a9e5c3a3eaf7db4328fd28f929ba3b1b6611511
ahmed abdallah <ahmedabdallah11186@gmail.com>
2024-04-28 10:34:15 +0300

    Create drawer_screen.dart

---
+++ lib/features/drawer/presentation/screens/drawer_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:maqam_v2/app/home_screen.dart';
+import 'package:maqam_v2/core/constants.dart';
+import 'package:maqam_v2/features/cart/presentation/view/screen.dart';
+import 'package:maqam_v2/features/search/presentation/view/search_screen.dart';
+import '../../../../config/network.dart';
+import '../controller/drawer_cubit.dart';
+import '../controller/drawer_state.dart';
+import 'package:maqam_v2/di_container.dart';
+import 'menu_screen.dart';
+
+class DrawerPage extends StatefulWidget {
+  const DrawerPage({super.key});
+
+  @override
+  State<DrawerPage> createState() => _DrawerPageState();
+}
+
+class _DrawerPageState extends State<DrawerPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    NetworkInfo.checkConnectivity(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<DrawerCubit, DrawerPageState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          final index = DrawerCubit.get(context).currentIndex;
+          return ZoomDrawer(
+            isRtl: false,
+            duration: const Duration(milliseconds: 250),
+            controller: zoomDrawerController,
+            style: DrawerStyle.style3,
+            menuScreen: const MenuScreen(),
+            mainScreen: [
+              const HomeScreen(),
+              const SearchScreen(),
+              const CartScreen(),
+            ][index],
+            mainScreenTapClose: false,
+            borderRadius: 70.0,
+            showShadow: true,
+            angle: 0,
+            menuBackgroundColor: Green,
+            slideWidth: MediaQuery.of(context).size.width * 0.75,
+          );
+        });
+  }
+}
