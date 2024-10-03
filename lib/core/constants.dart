8c078c9617f741acf60a3e586f96b8de75efcffa
ahmed abdallah <ahmedabdallah11186@gmail.com>
2024-10-03 13:25:54 +0300

    Update constants.dart

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -16,10 +16,26 @@ class HexColor extends Color {
   }
 }
 
-class AppColors{
+class AppColors {
   static var Green = HexColor('#059843');
   static var yellow = HexColor('#E1D800');
   static var gray = HexColor('#F7F7F7');
-  static final ZoomDrawerController zoomDrawerController = ZoomDrawerController();
+  static final ZoomDrawerController zoomDrawerController =
+      ZoomDrawerController();
 }
 
+class AppStrings {
+  AppStrings._();
+
+  static String appName = "Maqam";
+  static String exit = 'Press back again to exit';
+  static String boolNow = "Book Now";
+  static String alreadyInCart = "Already in cart";
+  static String notes = "This pricing is for 1 or 2 persons otherwise the pricing will be on each-one else ";
+  static String note = "Note";
+  static String seeAll = 'See All';
+  static String search = 'Search';
+  static String location = "Locations";
+  static String popularNow = 'Popular Trips';
+  static String recommendedTrips = 'Recommended Trips';
+}
