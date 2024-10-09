71b81dee5db12d7f7dd1272bbb0c2d8665fec189
ahmed abdallah <ahmedabdallah11186@gmail.com>
2024-10-09 19:04:34 +0300

    Update dummy_modules.dart

---
+++ lib/core/dummy_modules.dart
@@ -0,0 +1,28 @@
+import '../features/modules/data/models/module_model.dart';
+
+final modulesList = [
+  const ModuleModel(
+    name: "John Doe",
+    email: "johndoe@example.com",
+    phoneNumber: "+1234567890",
+    imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dbusiness%2Blogo&psig=AOvVaw18fIO8ncQXcxlNfAtJYQOt&ust=1728568344088000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLjhh4S5gYkDFQAAAAAdAAAAABAE",
+    backgroundImageUrl: "https://example.com/images/bg1.png",
+    id: "1",
+  ),
+  const ModuleModel(
+    name: "Jane Smith",
+    email: "janesmith@example.com",
+    phoneNumber: "+0987654321",
+    imageUrl: "https://www.google.com/imgres?q=companies%20logo%20images%20png&imgurl=https%3A%2F%2Fpng.pngtree.com%2Fpng-clipart%2F20190604%2Foriginal%2Fpngtree-creative-company-logo-png-image_1197025.jpg&imgrefurl=https%3A%2F%2Fpngtree.com%2Ffreepng%2Fcreative-company-logo_1197025.html&docid=sE5cdEkrSbPtoM&tbnid=b9bNEmZFB6nCKM&vet=12ahUKEwjuy_uAuYGJAxUxRPEDHR0EMjkQM3oECG4QAA..i&w=1200&h=1200&hcb=2&ved=2ahUKEwjuy_uAuYGJAxUxRPEDHR0EMjkQM3oECG4QAA",
+    backgroundImageUrl: "https://example.com/images/bg2.png",
+    id: "2",
+  ),
+  const ModuleModel(
+    name: "Alex Johnson",
+    email: "alexjohnson@example.com",
+    phoneNumber: "+1122334455",
+    imageUrl: "https://www.google.com/imgres?q=companies%20logo%20images%20png&imgurl=https%3A%2F%2Fpng.pngtree.com%2Fpng-clipart%2F20190604%2Foriginal%2Fpngtree-creative-company-logo-png-image_1197025.jpg&imgrefurl=https%3A%2F%2Fpngtree.com%2Ffreepng%2Fcreative-company-logo_1197025.html&docid=sE5cdEkrSbPtoM&tbnid=b9bNEmZFB6nCKM&vet=12ahUKEwjuy_uAuYGJAxUxRPEDHR0EMjkQM3oECG4QAA..i&w=1200&h=1200&hcb=2&ved=2ahUKEwjuy_uAuYGJAxUxRPEDHR0EMjkQM3oECG4QAA",
+    backgroundImageUrl: "https://example.com/images/bg3.png",
+    id: "3",
+  )
+];
